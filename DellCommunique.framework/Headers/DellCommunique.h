c9e63421ecdab45458c103d280cfdc5c56f887bb
Priya <priyas10@inenpriyas10m1.corp.emc.com>
2019-03-13 15:30:08 +0530

    first commit

---
+++ DellCommunique.framework/Headers/DellCommunique.h
@@ -0,0 +1,17 @@
+//
+//  DellCommunique.h
+//  DellCommunique
+//
+//  Created by Byan, Bharat on 13/11/18.
+//  Copyright © 2018 Byan, Bharat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DellCommunique.
+FOUNDATION_EXPORT double DellCommuniqueVersionNumber;
+
+//! Project version string for DellCommunique.
+FOUNDATION_EXPORT const unsigned char DellCommuniqueVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DellCommunique/PublicHeader.h>
